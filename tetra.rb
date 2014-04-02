1f0110117047fce0c19b73fd1596c085d1230042
anto1nee <gely.antoine@gmail.com>
2014-04-02 17:46:44 +0200

    First test ok

---
+++ tetra.rb
@@ -0,0 +1,24 @@
+# tetra.rb
+
+def rotationRight(tab)
+    
+    return tab.reverse.transpose
+    
+end
+
+
+def rotationLeft(tab)
+    
+    tab = tab.reverse.transpose
+    tab = tab.reverse.transpose
+    return tab.reverse.transpose
+    
+end
+
+
+def canRotateRight(tab)
+    
+    
+    return tab.reverse.transpose
+    
+end
